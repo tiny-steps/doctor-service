cae351e0652e95066d4de8624643a84ac69ea705
subh-m23 <subh.m@sascoders.com>
2025-08-28 20:41:48 +0530

    Updated ApplicationSecurityConfig to use a specific component name "doctorSecurity" for improved clarity in security configuration.

--- src/main/java/com/tinysteps/doctorsevice/config/ApplicationSecurityConfig.java
+++ src/main/java/com/tinysteps/doctorsevice/config/ApplicationSecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Component
+@Component("doctorSecurity")
 public class ApplicationSecurityConfig {
 
     private final DoctorRepository doctorRepository;
